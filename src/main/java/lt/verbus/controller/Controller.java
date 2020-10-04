1a1820d6f8edf09af626de041c741d94d1a63dd7
Sarunas Verbus <sarunas@verbus.lt>
2020-10-04 18:53:02 +0300

    getting ready for scene builder

--- src/main/java/lt/verbus/controller/Controller.java
+++ src/main/java/lt/verbus/controller/Controller.java
@@ -1,4 +1,14 @@
 package lt.verbus.controller;
 
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    public Button button1;
+
+    public void button1Clicked(){
+        System.out.println("Hello hello, hello...");
+        button1.setText("Stop touching me");
+    }
+
 }
